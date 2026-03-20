a045c40c02968f5aed54c0e611404956c603168d
Lucas Kuzma <lucas@lucaskuzma.com>
2026-03-20 17:13:36 +0900

    compose less

--- compose.bat
+++ compose.bat
@@ -1,15 +1,14 @@
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1233 --scale iwato 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1677 --scale iwato 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1871 --scale iwato 
-python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1950 --scale iwato 
+@REM python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1950 --scale iwato 
 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1234 --scale in-sen 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1678 --scale in-sen 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1872 --scale in-sen 
-python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1951 --scale in-sen
+@REM python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1951 --scale in-sen
 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1235 --scale kumoi 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1679 --scale kumoi 
 python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1873 --scale kumoi 
-python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1952 --scale kumoi
-
+@REM python3 evolve.py --encoding pitch --eval basic --generations 88 --seed 1952 --scale kumoi
